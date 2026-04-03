d55e67e74f1fb0ef3b5dfbdc726611b3bb677ad2
Oakland Evans <oaklandevans@yahoo.com>
2026-04-03 16:48:47 -0500

    Add .nojekyll file to fix GitHub Pages deployment

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,17 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { writeFileSync } from 'fs'
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    {
+      name: 'create-nojekyll',
+      closeBundle() {
+        writeFileSync('dist/.nojekyll', '')
+      }
+    }
+  ],
   base: '/',
   build: {
     outDir: 'dist'
